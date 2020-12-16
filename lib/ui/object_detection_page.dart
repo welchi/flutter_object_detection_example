c998c8a269da38de38c42673bd2b5c584729902c
welchi <welchizm@gmail.com>
2020-12-16 16:47:17 +0900

    Add object_detection_page

---
+++ lib/ui/object_detection_page.dart
@@ -0,0 +1,126 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_playground/data/model/controllers/controllers.dart';
+import 'package:flutter_playground/data/model/entities/entities.dart';
+import 'package:flutter_playground/data/model/model.dart';
+import 'package:flutter_playground/util/logger.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class BoundingBox extends HookWidget {
+  const BoundingBox(
+    this.result,
+  );
+  final Recognition result;
+
+  @override
+  Widget build(BuildContext context) {
+    final odController = useProvider(
+      objectDetectionControllerProvider,
+    );
+    final renderLocation = result.getRenderLocation(
+      odController.mlCamera.actualPreviewSize,
+      odController.mlCamera.ratio,
+    );
+    return Positioned(
+      left: renderLocation.left,
+      top: renderLocation.top,
+      width: renderLocation.width,
+      height: renderLocation.height,
+      child: Container(
+        width: renderLocation.width,
+        height: renderLocation.height,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.red,
+            width: 3,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(2),
+          ),
+        ),
+        child: buildBoxLabel(result),
+      ),
+    );
+  }
+
+  Align buildBoxLabel(Recognition result) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: FittedBox(
+        child: ColoredBox(
+          color: Colors.blue,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                result.label,
+              ),
+              Text(
+                ' ${result.score.toStringAsFixed(2)}',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ObjectDetectionPage extends HookWidget {
+  static String routeName = '/object_detection';
+
+  @override
+  Widget build(BuildContext context) {
+    final recognitions = useProvider(recognitionsProvider);
+    final size = MediaQuery.of(context).size;
+    logger.info('mediaSize0: ${size.toString()}');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Object Detection'),
+      ),
+      body: Stack(
+        children: [
+          const _CameraView(),
+          buildBoxes(
+            recognitions.state,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildBoxes(
+    List<Recognition> recognitions,
+  ) {
+    if (recognitions == null || recognitions.isEmpty) {
+      return const SizedBox();
+    }
+    return Stack(
+      children: recognitions.map((result) {
+        return BoundingBox(result);
+      }).toList(),
+    );
+  }
+}
+
+class _CameraView extends HookWidget {
+  const _CameraView({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final odController = useProvider(
+      objectDetectionControllerProvider,
+    );
+    final size = MediaQuery.of(context).size;
+    odController.mlCamera.initScreenInfo(size);
+    logger.info('mediaSize: ${size.toString()}');
+    return AspectRatio(
+      aspectRatio: odController.mlCamera.cameraController.value.aspectRatio,
+      child: CameraPreview(
+        odController.mlCamera.cameraController,
+      ),
+    );
+  }
+}
